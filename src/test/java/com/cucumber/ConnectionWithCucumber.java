98feef62141eaa54b069eba56409d92305e97734
JohnneSouza <jhonnesf5@gmail.com>
2020-08-18 01:09:46 -0300

    Class folder refactor

--- src/test/java/com/cucumber/ConnectionWithCucumber.java
+++ src/test/java/com/cucumber/ConnectionWithCucumber.java
@@ -1,34 +1,17 @@
 package com.cucumber;
 
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.http.Header;
-import org.junit.Rule;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@CucumberContextConfiguration
-@AutoConfigureWireMock
 public class ConnectionWithCucumber {
-
-    @Value("${another.server.port}")
-    private int PORT;
-
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(options().port(PORT));
 
     private String messageResult;
 
--- src/test/java/com/cucumber/RunCucumberTest.java
+++ src/test/java/com/cucumber/RunCucumberTest.java
@@ -1,10 +1,24 @@
 package com.cucumber;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 @RunWith(Cucumber.class)
-@CucumberOptions()
+@CucumberContextConfiguration
+@AutoConfigureWireMock
 public class RunCucumberTest {
+
+    @Value("${another.server.port}")
+    private int PORT;
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(options().port(PORT));
+
 }

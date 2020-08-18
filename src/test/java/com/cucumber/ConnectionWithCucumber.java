7652664f2603ec2685f72746ba55392184460f62
JohnneSouza <jhonnesf5@gmail.com>
2020-08-18 00:39:14 -0300

    Spring Cloud Wiremock

--- src/test/java/com/cucumber/ConnectionWithCucumber.java
+++ src/test/java/com/cucumber/ConnectionWithCucumber.java
@@ -1,51 +1,46 @@
 package com.cucumber;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.http.Header;
 import org.junit.Rule;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @CucumberContextConfiguration
+@AutoConfigureWireMock
 public class ConnectionWithCucumber {
 
     @Value("${another.server.port}")
     private int PORT;
 
     @Rule
-    private WireMockRule wireMockRule = new WireMockRule(PORT);
-
-    private WireMockServer wireMockServer = new WireMockServer(PORT);
-
+    public WireMockRule wireMockRule = new WireMockRule(options().port(PORT));
 
     private String messageResult;
 
     @Given("I want to read a message")
     public void iWantToRead() {
-        wireMockServer.start();
-        wireMockRule.start();
-        wireMockRule.isRunning();
-        wireMockServer.isRunning();
-        // Start the stub
-        createMessageStubServer();
         createMessageStub();
-
-        wireMockServer.getStubMappings();
-        wireMockRule.getStubMappings();
     }
 
     @When("I send the request")
     public void iSendTheRequest() {
-        messageResult = given().get("localhost:8082/message").getBody().asString();
+        Header header = new Header("Content-Type","application/json");
+        messageResult = given().header(header).get("/message").getBody().asString();
     }
 
     @Then("I should be able to read the word {string}")
@@ -54,20 +49,10 @@ public class ConnectionWithCucumber {
     }
 
     private void createMessageStub() {
-        wireMockRule.stubFor(get(urlEqualTo("/message"))
-                .withHeader("Accept", equalTo("application/json"))
+        stubFor(get(urlEqualTo("/message"))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
-                        .withBody("message")));
-    }
-
-    private void createMessageStubServer() {
-        wireMockServer.stubFor(get(urlEqualTo("/message"))
-                .withHeader("Accept", equalTo("application/json"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody("message")));
+                        .withBody("Response")));
     }
 }
